fb2c7e572e243826fc32fc8e8a9404c7cd2f7be2
renanmalato <malatows@gmail.com>
2025-03-23 23:40:39 -0400

    ✅️ Navbar Desktop / ✅️ Routes COnfig

---
+++ src/app/(pages)/packages/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+
+import { NavBar, Packages, Footer } from '@/sections'
+
+export default function PackagesPage() {
+    return (
+        <div>
+            <NavBar />
+            <Packages />
+            <Footer />
+        </div>
+    )
+}
--- src/app/sections/navbar/NavBar.tsx
+++ src/app/sections/navbar/NavBar.tsx
@@ -3,6 +3,7 @@
 import './NavBar.scss';
 import { useScroll } from '@/context/ScrollContext';
 import { LanguageSelector } from './components/LanguageSelector/LanguageSelector';
+import Image from 'next/image';
 
 export default function NavBar() {
   const { scrollToSection } = useScroll();
@@ -16,27 +17,44 @@ export default function NavBar() {
   ];
 
   return (
-    <nav className="navbar">
-      <div className="logo">
-        <h3>MUSICA</h3>
+    <div className="navbar-container">
+
+    <div className="logo-container">
+      <div className="logos-left">
+        <Image src="/assets/img/theknotlogo.avif" alt="logo" width={100} height={30} />
+        <Image src="/assets/img/weddingwirelogo.avif" alt="logo" width={70} height={30} />
       </div>
+
+      <div className="renanmalato">
+      <span className="text-top">RENAN MALATO</span>
+      <span className="text-bottom">#GOODMUSICONLY</span>
+      </div>
+
+      <div className="logos-right">
+        <Image src="/assets/img/gig-salad-badge-top-performer.avif" alt="logo" width={100} height={70} />
+              {/* Language Selector */}
+      <div className="language-selector-container">
+        <LanguageSelector />
+      </div>
+      </div>
+    </div>
+    
+    <nav className="navbar">
+
       <ul className="nav-links">
+
         {sections.map((section) => (
           <li key={section.name}>
-            <button 
-              onClick={() => scrollToSection(section.name)}
-              className="nav-link"
-            >
+            <button onClick={() => scrollToSection(section.name)} className="nav-link">
               {section.label}
             </button>
           </li>
         ))}
+
       </ul>
       
-      {/* Language Selector */}
-      <div className="language-selector-container">
-        <LanguageSelector />
-      </div>
+
     </nav>
+    </div>
   );
 } 
